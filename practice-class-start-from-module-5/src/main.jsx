876637ced59119121f2cc54e76f9b671db3d3df9
Shamiul40 <shamiul4040@gmail.com>
2026-01-07 21:55:58 +0600

    config

--- practice-class-start-from-module-5/src/main.jsx
+++ practice-class-start-from-module-5/src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './Root'
 
 const router = createBrowserRouter([
   {
     path : "/",
-    Component : 
+    element : <Root></Root>
   }
 ])
 
