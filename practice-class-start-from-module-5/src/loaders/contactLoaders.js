b087d61ec8beae33a7af3b7aac549a31c0460a8d
Shamiul40 <shamiul4040@gmail.com>
2026-01-08 22:12:22 +0600

    contact loader

--- practice-class-start-from-module-5/src/loaders/contactLoaders.js
+++ practice-class-start-from-module-5/src/loaders/contactLoaders.js
@@ -4,3 +4,8 @@ export async function getContactsLoader() {
   const contacts = await getContacts();
   return { contacts };
 }
+
+export async function getContactLoader({params}) {
+  const contacts = await getContacts(params.contactsId);
+  return { contacts };
+}
