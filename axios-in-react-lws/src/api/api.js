c959af8ae5149ddd4b1efb255e36fe2094cb1f4b
Shamiul40 <shamiul4040@gmail.com>
2026-01-11 21:22:50 +0600

    interceptors response

--- axios-in-react-lws/src/api/api.js
+++ axios-in-react-lws/src/api/api.js
@@ -3,4 +3,24 @@ import axios from "axios"
 export const api =axios.create({
     baseURL : "http://localhost:8000",
     
+    
 })
+
+
+api.interceptors.request.use((config)=>{
+    config.headers["Authorization"] = "Bearer aaaaaaaaaaaaaaaaaaa"
+    console.log(config)
+    return config
+}, (error)=>{
+    return Promise.reject(error)
+})
+
+
+
+api.interceptors.response.use((response)=>{
+    console.log(response)
+    return response
+}, (error)=>{
+    Promise.reject(error)
+}
+)
