870649711af0d8d4cc5decf4e30dd61e0e284496
Shamiul40 <shamiul4040@gmail.com>
2026-01-13 20:29:35 +0600

    product details

--- Use Query/use-react-query-module-5.3/src/Components/ProductDetails.jsx
+++ Use Query/use-react-query-module-5.3/src/Components/ProductDetails.jsx
@@ -1,3 +1,4 @@
+import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import React from 'react'
 
@@ -7,9 +8,17 @@ const retriveProduct = async()=>{
 }
 
 export default function ProductDetails() {
+
+    const{data : product, isLoading, error} = useQuery({
+        queryKey : ["products"],
+        queryFn : retriveProduct,
+
+    })
+
+   
   return (
     <div>
-      product details
+      
     </div>
   )
 }
