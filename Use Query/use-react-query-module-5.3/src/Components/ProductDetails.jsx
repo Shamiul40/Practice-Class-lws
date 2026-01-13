b1efb03810d25fd55d745d91330e079d616d21a7
Shamiul40 <shamiul4040@gmail.com>
2026-01-13 20:34:24 +0600

    add

--- Use Query/use-react-query-module-5.3/src/Components/ProductDetails.jsx
+++ Use Query/use-react-query-module-5.3/src/Components/ProductDetails.jsx
@@ -15,10 +15,21 @@ export default function ProductDetails() {
 
     })
 
-   
+   if(isLoading) return <div>data fetching...</div>
+   if(error) return <div>data not found ${error.message}</div>
   return (
-    <div>
-      
+     <div className='w-1/5'>
+        <h1 className="text-3xl my-2">Product Details</h1>
+        <div className='border bg-gray-100 p-1 text-md rounded flex flex-col'>
+            <img
+                src={product.thumbnail}
+                alt={product.title}
+                className="object-cover h-24 w-24 border rounded-full m-auto" />
+            <p>{product.title}</p>
+            <p>{product.description}</p>
+            <p>USD {product.price}</p>
+            <p>{product.rating}/5</p>
+        </div>
     </div>
   )
 }
