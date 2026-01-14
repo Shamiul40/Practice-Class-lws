2f61914b9040e0ef1857baf7562b3a5f77c367a8
Shamiul40 <shamiul4040@gmail.com>
2026-01-14 22:35:19 +0600

    handle input data

--- react-hook-form/src/Form/LoginForm.jsx
+++ react-hook-form/src/Form/LoginForm.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Fieldset from "../Components/Fieldset";
 import Field from "../Components/Field";
+import { useForm } from "react-hook-form";
 
 export default function LoginForm() {
+
+    const {register, handleSubmit} =useForm()
+    const handleInput=(data)=>{
+        console.log(data)
+    }
   return (
     <div className="m-4">
-      <form>
+      <form onSubmit={handleSubmit(handleInput)}>
         <Fieldset lable={"React hook form"}>
           <Field lable={"email"}>
             <input
-            name="email"
+            {...register("email")}
+              id="email"
+              name="email"
               type="email"
               placeholder="type your email"
               className=" block w-3/12 rounded-lg border-gray-300 px-4 py-2 text-gray-900 shadow-sm sm:text-sm"
@@ -18,14 +26,18 @@ export default function LoginForm() {
 
           <Field lable={"password"}>
             <input
-                name="password"
+            {...register("password")}
+              id="password"
+              name="password"
               type="password"
               placeholder="type your password"
               className=" block w-3/12 rounded-lg border-gray-300 px-4 py-2 text-gray-900 shadow-sm sm:text-sm"
             />
           </Field>
           <Field>
-            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg">Submit</button>
+            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg">
+              Submit
+            </button>
           </Field>
         </Fieldset>
       </form>
