91abe2a613b7b5c5bb094c2c89e458fb0e6deac7
Shamiul40 <shamiul4040@gmail.com>
2026-01-14 22:13:43 +0600

    htmlFor use in input instead of id and make a function to get htmlFor id

--- react-hook-form/src/Components/Field.jsx
+++ react-hook-form/src/Components/Field.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
-export default function Field({lable, children, error}) {
+export default function Field({lable, children, htmlFor, error}) {
+
+    const id = htmlFor || getHtmlForId(children)
+
   return (
     <div className='flex flex-col justify-start items-start gap-2'>
-        {lable && <div>{lable}</div>} 
+        {lable && <label htmlFor={id}>{lable}</label>} 
         {children}
         {error && <div>{error.message}</div>}
     </div>
   )
+}
+
+
+const getHtmlForId=(children)=>{
+    const child = React.Children.only(children);
+
+    if("id" in child?.props) {
+        return child.props.id
+    }
 }
