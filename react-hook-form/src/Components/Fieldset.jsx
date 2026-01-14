25c150de1a5b91f09a975b54f3e0d5a3a4f29743
Shamiul40 <shamiul4040@gmail.com>
2026-01-14 20:31:27 +0600

    email and password input added

--- react-hook-form/src/Components/Fieldset.jsx
+++ react-hook-form/src/Components/Fieldset.jsx
@@ -4,8 +4,8 @@ export default function Fieldset({lable, children}) {
 
   return (
 
-      <div className='border border-gray-300 p-4 rounded-lg '>
-        {lable && <legend className='text-gray-700'>{lable}</legend>}
+      <div className='border border-gray-300 p-4 rounded-lg flex  flex-col gap-4 '>
+        {lable && <legend className=' text-3xl mb-3'>{lable}</legend>}
         {children}
       </div>
   )
--- react-hook-form/src/Form/LoginForm.jsx
+++ react-hook-form/src/Form/LoginForm.jsx
@@ -4,12 +4,28 @@ import Field from "../Components/Field";
 
 export default function LoginForm() {
   return (
-    <div>
+    <div >
       <Fieldset lable={"React hook form"}>
         <Field lable={"email"}>
           <input
             type="text"
             placeholder="type your email"
+            className=" block
+    w-3/12
+    rounded-lg
+    border-gray-300
+    px-4
+    py-2
+    text-gray-900
+    shadow-sm
+   
+    sm:text-sm"
+          />
+        </Field>
+        <Field lable={"password"}>
+          <input
+            type="password"
+            placeholder="type your password"
             className=" block
     w-3/12
     rounded-lg
