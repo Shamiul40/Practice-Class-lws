6d1fe271c28657640c76e7872db888415daba57a
Shamiul40 <shamiul4040@gmail.com>
2026-01-14 20:48:24 +0600

    button added

--- react-hook-form/src/Components/Field.jsx
+++ react-hook-form/src/Components/Field.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Field({lable, children, error}) {
   return (
     <div className='flex justify-start items-center gap-4'>
-        {lable && <div>{lable}</div>} :
+        {lable && <div>{lable}</div>} 
         {children}
         {error && <div>{error.message}</div>}
     </div>
--- react-hook-form/src/Form/LoginForm.jsx
+++ react-hook-form/src/Form/LoginForm.jsx
@@ -4,41 +4,29 @@ import Field from "../Components/Field";
 
 export default function LoginForm() {
   return (
-    <div >
-      <Fieldset lable={"React hook form"}>
-        <Field lable={"email"}>
-          <input
-            type="text"
-            placeholder="type your email"
-            className=" block
-    w-3/12
-    rounded-lg
-    border-gray-300
-    px-4
-    py-2
-    text-gray-900
-    shadow-sm
-   
-    sm:text-sm"
-          />
-        </Field>
-        <Field lable={"password"}>
-          <input
-            type="password"
-            placeholder="type your password"
-            className=" block
-    w-3/12
-    rounded-lg
-    border-gray-300
-    px-4
-    py-2
-    text-gray-900
-    shadow-sm
-   
-    sm:text-sm"
-          />
-        </Field>
-      </Fieldset>
+    <div className="m-4">
+      <form>
+        <Fieldset lable={"React hook form"}>
+          <Field lable={"email"}>
+            <input
+              type="text"
+              placeholder="type your email"
+              className=" block w-3/12 rounded-lg border-gray-300 px-4 py-2 text-gray-900 shadow-sm sm:text-sm"
+            />
+          </Field>
+
+          <Field lable={"password"}>
+            <input
+              type="password"
+              placeholder="type your password"
+              className=" block w-3/12 rounded-lg border-gray-300 px-4 py-2 text-gray-900 shadow-sm sm:text-sm"
+            />
+          </Field>
+          <Field>
+            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg">Submit</button>
+          </Field>
+        </Fieldset>
+      </form>
     </div>
   );
 }
