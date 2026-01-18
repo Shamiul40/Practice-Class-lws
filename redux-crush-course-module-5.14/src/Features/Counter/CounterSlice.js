9dcdf3a9b1ce9ef14ba6c83a72baffac216eee96
Shamiul40 <shamiul4040@gmail.com>
2026-01-19 00:24:10 +0600

    create slice set up

---
+++ redux-crush-course-module-5.14/src/Features/Counter/CounterSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialCounters = [
+  {
+    id: 1,
+    value: 0
+  },
+  {
+    id: 2,
+    value: 0
+  }
+]
+
+const counterSlice = createSlice()
