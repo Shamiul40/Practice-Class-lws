2f5ef78eb0c8f2d2fea7bdae2f052621411ba2ad
Shamiul40 <shamiul4040@gmail.com>
2026-01-18 22:21:33 +0600

    bug fix

--- redux-crush-course-module-5.14/src/App.jsx
+++ redux-crush-course-module-5.14/src/App.jsx
@@ -1,14 +1,58 @@
 
+import { useState } from 'react'
 import './App.css'
 import Counter from './Components/Counter'
 
+
+const initialCounters = [
+  {
+    id: 1,
+    value: 0
+  },
+  {
+    id: 2,
+    value: 0
+  }
+]
+
 function App() {
+const [counters, setCounters] = useState(initialCounters)
+
+
+  const handleIncrement = (id) => {
+    setCounters(counters.map(counter=>{
+      if(counter.id===id){
+        return {
+          ...counter,
+          value: counter.value + 1
+        }
+      }
+      return counter;
+    }))
+  }
+
+  const handleDecrement = (id) => {
+    setCounters(counters.map(counter=>{
+      if(counter.id===id){
+        return {
+          ...counter,
+          value: counter.value - 1
+        }
+      }
+      return counter;
+    }))
+  }
+ 
   
 
   return (
     <div className='flex justify-center flex-col items-center gap-5 mt-10'>
     <h1 className='text-2xl'>Simple Counter Application</h1>
-      <Counter></Counter>
+      {
+        counters.map((counter)=>(
+          <Counter key={counter.id} count={counter.value} handleIncrement={()=>handleIncrement(counter.id)} handleDecrement={()=>handleDecrement(counter.id)}></Counter>
+        ))
+      }
     </div>
   )
 }
--- redux-crush-course-module-5.14/src/Components/Counter.jsx
+++ redux-crush-course-module-5.14/src/Components/Counter.jsx
@@ -2,23 +2,15 @@ import React, { useState } from "react";
 import Count from "./Count";
 import Button from "./Button";
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
-
-  const handleIncrement=()=>{
-    setCount(prevCount=>prevCount+1)
-  }
+export default function Counter({count, handleIncrement, handleDecrement}) {
   
-  const handleDecrement=()=>{
-    setCount(prevCount=>prevCount-1)
-  }
 
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div className="flex flex-col items-center gap-4 bg-white p-6 rounded shadow">
       <Count count={count}></Count>
       <div className="flex justify-start gap-3">
-        <Button handler={handleIncrement}>Increment</Button>
-        <Button type="danger" handler={handleDecrement}>Decrement</Button>
+        <Button type="danger"  handler={handleIncrement}>Increment</Button>
+        <Button handler={handleDecrement}>Decrement</Button>
       </div>
     </div>
   );
