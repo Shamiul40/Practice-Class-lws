cbb6ed815de527910bf8a0e022752124776face1
Shamiul40 <shamiul4040@gmail.com>
2026-01-19 00:32:40 +0600

    state update

--- redux-crush-course-module-5.14/src/Features/Counter/CounterSlice.js
+++ redux-crush-course-module-5.14/src/Features/Counter/CounterSlice.js
@@ -1,15 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const initialCounters = [
   {
     id: 1,
-    value: 0
+    value: 0,
   },
   {
     id: 2,
-    value: 0
-  }
-]
+    value: 0,
+  },
+];
 
-const counterSlice = createSlice()
+const counterSlice = createSlice({
+  name: counters,
+  initialCounters,
+  reducers: {
+    increment: (state, action) => {
+      const counterIndex = state.findIndex((c) => c.id === action.payload);
+      state[counterIndex].value++
+    },
+    decrement: (state, action) => {},
+  },
+});
